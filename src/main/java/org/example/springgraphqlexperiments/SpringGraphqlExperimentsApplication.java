07ff5a66997e819d6130645f4fa363d434979e78
Felipe Reis <freis@atlassian.com>
2024-02-14 08:35:33 +1100

    Running example

---
+++ src/main/java/org/example/springgraphqlexperiments/SpringGraphqlExperimentsApplication.java
@@ -0,0 +1,13 @@
+package org.example.springgraphqlexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGraphqlExperimentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springgraphqlexperiments/SpringGraphqlExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springgraphqlexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlExperimentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
